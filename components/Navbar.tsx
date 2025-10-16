c2337df5df7a2a9f52547bef8f569e37e58a02d3
Ari Sudana <arisudana114@gmail.com>
2025-10-16 16:23:57 +0700

    adding portofolio page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="sticky top-0 z-50 w-full bg-white shadow-sm py-4 flex justify-center items-center">
-      <div className="relative w-20 h-20 md:w-32 md:h-32">
+      <div className="relative w-16 h-16 md:w-24 md:h-24">
         <Link href="/">
           <Image
             src="/saltandlight.png"
